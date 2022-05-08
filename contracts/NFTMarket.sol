c11d8f65e97856a9306725932c8febb0ed06c8b6
Xeragus <bobansugareski>
2022-05-08 21:30:21 +0200

    Add NFT & NFTMarket contracts, passing test

---
+++ contracts/NFTMarket.sol
@@ -0,0 +1,176 @@
+// SPDX-License-Identifier: MIT OR Apache-2.0
+pragma solidity ^0.8.3;
+
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
+
+contract NFTMarket is ReentrancyGuard {
+    using Counters for Counters.Counter;
+    Counters.Counter private _itemIds;
+    Counters.Counter private _itemsSold;
+
+    address payable owner;
+    uint256 tax = 0.025 ether;
+
+    struct MarketItem {
+        uint itemId;
+        uint256 tokenId;
+        uint256 price;
+        address nftContractAddress;
+        address payable seller;
+        address payable owner;
+        bool sold;
+    }
+
+    mapping(uint256 => MarketItem) private idToMarketItemMap;
+
+    event MarketItemCreated (
+        uint indexed itemId,
+        uint256 indexed tokenId,
+        uint256 price,
+        address indexed nftContractAddress,
+        address seller,
+        address owner,
+        bool sold
+    );
+
+    constructor() {
+        owner = payable(msg.sender);
+    }
+
+    function getTax() public view returns (uint256) {
+        return tax;
+    }
+
+    function createMarketItem(
+        address nftContractAddress,
+        uint256 tokenId,
+        uint256 price
+    ) public payable nonReentrant {
+        // nonReentrant modifier used
+        require(price > 0, "Price must be at least 1 wei");
+        require(
+            msg.value == tax,
+            string(
+                abi.encodePacked(
+                    "The paid tax amount has to be equal to the Minimal Solidarity Tax amount of ",
+                    tax,
+                    ' ether'
+                )   
+            )
+        );
+
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
+        idToMarketItemMap[itemId] = MarketItem(
+            itemId,
+            tokenId,
+            price,
+            nftContractAddress,
+            payable(msg.sender),
+            payable(address(0)),
+            false
+        );
+
+        IERC721(nftContractAddress).transferFrom(msg.sender, address(this), tokenId);
+    }
+
+    function createMarketSale(
+        address nftContractAddress,
+        uint256 itemId
+    ) public payable nonReentrant{
+        uint price = idToMarketItemMap[itemId].price;
+        uint256 tokenId = idToMarketItemMap[itemId].tokenId;
+        console.log(msg.value, tax);
+        require(
+            msg.value == tax,
+            string(
+                abi.encodePacked(
+                    "The price you are trying to pay has to be equal with the price of the product ",
+                    price,
+                    ' ether'
+                )   
+            )
+        );
+
+        idToMarketItemMap[itemId].seller.transfer(msg.value);
+        IERC721(nftContractAddress).transferFrom(address(this), msg.sender, tokenId);
+        idToMarketItemMap[itemId].owner = payable(msg.sender);
+        idToMarketItemMap[itemId].sold = true;
+        _itemsSold.increment();
+        payable(owner).transfer(tax);
+    }
+
+    function fetchMarketItems() public view returns(MarketItem[] memory) {
+        uint itemsCount = _itemIds.current();
+        uint unsoldItemsCount = _itemIds.current() - _itemsSold.current();
+        uint currentIndex = 0;
+
+        MarketItem[] memory items = new MarketItem[](unsoldItemsCount);
+
+        for(uint i = 0; i < itemsCount; i++) {
+            if (idToMarketItemMap[i + 1].owner == address(0)) {
+                uint currentItemId = idToMarketItemMap[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItemMap[currentItemId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return items;
+    }
+
+    function fetchMyPurchasedNFTs() public view returns(MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint myPurchasedNFTsCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItemMap[i + 1].owner == msg.sender) {
+                myPurchasedNFTsCount += 1; 
+            }
+        }
+
+        MarketItem[] memory myPurchasedNFTs = new MarketItem[](myPurchasedNFTsCount);
+        
+        for(uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItemMap[i + 1].owner == msg.sender) {
+                uint currentId = idToMarketItemMap[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItemMap[currentId];
+                myPurchasedNFTs[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return myPurchasedNFTs;
+    }
+
+    function fetchMyCreatedNFTs() public view returns(MarketItem[] memory) {
+        uint totalItemCount = _itemIds.current();
+        uint myCreatedNFTsCount = 0;
+        uint currentIndex = 0;
+
+        for(uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItemMap[i + 1].seller == msg.sender) {
+                myCreatedNFTsCount += 1; 
+            }
+        }
+
+        MarketItem[] memory myCreatedNFTs = new MarketItem[](myCreatedNFTsCount);
+        
+        for(uint i = 0; i < totalItemCount; i++) {
+            if (idToMarketItemMap[i + 1].seller == msg.sender) {
+                uint currentId = idToMarketItemMap[i + 1].itemId;
+                MarketItem storage currentItem = idToMarketItemMap[currentId];
+                myCreatedNFTs[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+
+        return myCreatedNFTs;
+    }
+}
--- test/sample-test.js
+++ test/sample-test.js
@@ -1,19 +1,44 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+describe("NFTMarket", function() {
+  it("Should create and execute market sales", async function() {
+    const Market = await ethers.getContractFactory("NFTMarket")
+    const market = await Market.deploy()
+    await market.deployed()
+    const marketAddress = market.address
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+    const NFT = await ethers.getContractFactory("NFT")
+    const nft = await NFT.deploy(marketAddress)
+    await nft.deployed()
+    const nftContractAddress = nft.address
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+    let tax = await market.getTax();
+    console.log('Tax', tax);
+    tax = tax.toString();
+    console.log('Tax toString', tax);
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+    const auctionPrice = ethers.utils.parseUnits('0.025', 'ether')
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
+    await nft.createToken("https://www.mytokenlocation222223434343.com")
+    await nft.createToken("https://www.mytokenlocation434343434343332.com")
+  
+    await market.createMarketItem(nftContractAddress, 1, auctionPrice, { value: tax })
+    await market.createMarketItem(nftContractAddress, 2, auctionPrice, { value: tax })
+    
+    const [_, buyerAddress] = await ethers.getSigners()
 
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
+    await market.connect(buyerAddress).createMarketSale(nftContractAddress, 1, { value: auctionPrice})
+
+    items = await market.fetchMarketItems()
+    items = await Promise.all(items.map(async i => {
+      const tokenUri = await nft.tokenURI(i.tokenId)
+      let item = {
+        price: i.price.toString(),
+        tokenId: i.tokenId.toString(),
+        seller: i.seller,
+        owner: i.owner,
+        tokenUri
+      }
+      return item
+    }))
+    console.log('items: ', items)
+  })
 });
